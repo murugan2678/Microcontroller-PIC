a56dd5e8cb301e6f6d64bb7c6aebf1c41e83e8d8
murugan2678 <muruganm2947@gmail.com>
2023-01-24 14:53:13 +0530

    gsm

---
+++ gsm/gsm_phone_call.c
@@ -0,0 +1,124 @@
+/**************************************************************************/
+/* Auther       : Murugan M                                               */
+/* Date         : 13/1/2023                                               */
+/* File name    : gsm phone call                                          */
+/* Description  : gsm-global system for mobile communication using usart  */
+/**************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<htc.h>
+#define _XTAL_FREQ 20000000
+__CONFIG(0x1f7a);
+
+#define RS RD5                        //Register select pin
+#define RW RD6                        //Read and Write pin
+#define EN RD7                        //Enable pin
+
+void lcd_cmd(unsigned char cmd);      //unsigned means + value only print
+void lcd_data(unsigned char data);    //unsigned means + value only print
+void uart_tx(unsigned char val);      //uart transmitter character value
+unsigned char uart_rx(void);          //uart reciever 
+void uart_init(void);                 //uart_initialization
+void uart_init();
+void config_init();
+char AT[]="AT\r\n";                   //AT-Attension AT comments for gsm
+char sms_mode[]="AT+CMGF=1\r\n";      //sms sending mode AT+CMGF=1, \r\n - this means for new line enter
+
+char call_number[]="ATD9487855450;\r\n"; //ATD mobile call the number 
+
+
+void lcd_cmd(unsigned char cmd)       //unsigned means + value only print    
+{
+	RS = 0;                           //Register select this cmd 0 off
+	RW = 0;                           //Read and write this cmd 0 off
+	EN = 1;                           //Enable off
+	PORTB = cmd;                      //portable lcd command set function
+	__delay_ms(100);                  
+	EN = 0;                           //Enable off
+}
+void lcd_data(unsigned char data)     //unsigned means + value only print
+{
+	RS = 1;                           //Register select this data 1 means on
+	RW = 0;                           //Read and Write this data 0 off
+	EN = 1;                           //Enable on
+	PORTB = data;                     //portb lcd data going for lcd
+	__delay_ms(100);
+	EN = 0;                           //Enable off
+}
+
+void lcd_init()
+{
+	TRISB  = 0X00;                    //TRISB 8 pin enable send data
+	TRISD5= 0;                        //TRISD5 pin enable send data
+	TRISD6= 0;                        //TRISD6 pin enable send data   
+	TRISD7= 0;                        //TRISD7 pin enable send data
+	
+	lcd_cmd(0X38);                    //init 5X8-2 line RET HOME
+	lcd_cmd(0X0F);                    //display on,en_cursor,en_blink
+	lcd_cmd(0X0C);                    //display on,en_cursor,en_blink
+	lcd_cmd(0X02);                    //Ret to home
+	lcd_cmd(0X06);                    //cursor ++
+	lcd_cmd(0X80);                    //set DDRAM
+	__delay_ms(100);
+	lcd_cmd(0X01);                    //clear display
+}
+void uart_init()
+{
+	TRISC6 = 0;                       //Low 25 pin RC6/TX/DT   TX-transmitter DT-data
+	TRISC7 = 1;                       //HIGH 26 pin RC7/RX/CK  RX-receiver CK-clock wise
+	TXSTA  = 0X20;                    //data bit 5 TXEN = 1   data bit 5 enable for transmitter
+	RCSTA  = 0X90;                    //data bit 7 SPEN = 1 bit 4 CREN =1   data bit 7 and 4 enable SPEN=1 serial port enable CREN = 1 continous receiver enable bit 
+	SPBRG  = 31;                      //SPBRG serial port baud rate generator register(address 99h)
+	TXIF = 0;                         //TXIF transmitter inter flag set
+	RCIF = 0;                         //RCIF receiver inter flag set
+}
+void uart_tx(unsigned char val)       //transmitter unsigned means + value only print
+{
+    TXREG = val;                      //transmitter register
+    while(TXIF == 0);                 //transmitter buffer TXIF = 0
+    TXIF = 0;                         //transmit IF means inter flag set
+}
+unsigned char uart_rx(void)           //receiver unsigned means + value only print
+{
+    while(RCIF == 0);                 //poll is RCIF is empty RCIF-receiver inter flag
+    RCIF = 0;                         
+    return RCREG;                     //RCREG - receiver register
+}
+void uart_print(unsigned char *str)   //sring using char *strstr(const char *haystack, const char *needle)
+{
+    while(*str)                      
+    {
+       uart_tx(*str++);               //functions first occurence of the substring needle in the string haystack
+    }                                 //the terminating 0 characters are not compared
+}
+/*void lcd_print(unsigned char *str)  //lcd data print
+{
+    while(*str)
+    {
+       lcd_init(*str++);
+    }
+}*/
+int main()
+{
+    lcd_init();
+    uart_init();
+	{
+        uart_print(call_number);      //ATD mobile call the number
+        __delay_ms(3000);    
+        
+    while(1);
+	}
+   
+   
+}
